0ee4db7df4028bf4ed0b7ad04f153a53d5aa1d61
Janine Luk <janineluk95@gmail.com>
2018-11-26 09:19:15 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select {|appointment| appointment.patient == self}
+    Appointment.all.select {|date| date.patient == self}
   end
   
   def doctors
